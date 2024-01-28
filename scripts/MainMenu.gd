c6e45826172fe1cfc1116ad98f13afb0099a8376
Nono <noah.johanson@gmail.com>
2024-01-27 21:05:37 -0800

    Add music and SFX

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -5,6 +5,7 @@ signal start_game
 @onready var MainMenu = get_node("/root/MainMenu")
 
 func _on_start_pressed():
+	$buttonsound.play()
 	emit_signal("start_game")
 	MainMenu.hide()
 	get_tree().change_scene_to_file("res://scenes/level_map.tscn")
@@ -12,4 +13,5 @@ func _on_start_pressed():
 
 
 func _on_quit_pressed():
+	$buttonsound.play()
 	get_tree().quit()
